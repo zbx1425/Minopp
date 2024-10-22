c1e6be92aa692cb6d78683466b6fdf7d1036c255
Crystal1921 <1921331938@qq.com>
2024-10-22 22:21:39 +0800

    使用接口干掉了mixin

--- neoforge/src/main/java/cn/zbx1425/minopp/neoforge/compat/touhou_little_maid/MaidPlugin.java
+++ neoforge/src/main/java/cn/zbx1425/minopp/neoforge/compat/touhou_little_maid/MaidPlugin.java
@@ -3,6 +3,7 @@ package cn.zbx1425.minopp.neoforge.compat.touhou_little_maid;
 import cn.zbx1425.minopp.neoforge.compat.touhou_little_maid.task.MinoppTask;
 import com.github.tartaricacid.touhoulittlemaid.api.ILittleMaid;
 import com.github.tartaricacid.touhoulittlemaid.api.LittleMaidExtension;
+import com.github.tartaricacid.touhoulittlemaid.entity.ai.brain.ExtraMaidBrainManager;
 import com.github.tartaricacid.touhoulittlemaid.entity.task.TaskManager;
 
 @LittleMaidExtension
@@ -10,5 +11,10 @@ public class MaidPlugin implements ILittleMaid {
     @Override
     public void addMaidTask(TaskManager manager) {
         manager.add(new MinoppTask());
+    }
+
+    @Override
+    public void addExtraMaidBrain(ExtraMaidBrainManager manager) {
+        manager.addExtraMaidBrain(new MaidExtraBrain());
     }
 }
