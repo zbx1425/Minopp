5f5b8b086e99b275b0f82908fd0e257f75960f6f
zbx1425 <support@zbx1425.cn>
2025-01-12 11:43:40 +0800

    Fix getFieldOfViewModifier mixin compatibility

--- fabric/src/main/java/cn/zbx1425/minopp/fabric/mixin/AbstractClientPlayerMixin.java
+++ fabric/src/main/java/cn/zbx1425/minopp/fabric/mixin/AbstractClientPlayerMixin.java
@@ -1,17 +1,16 @@
 package cn.zbx1425.minopp.fabric.mixin;
 
 import cn.zbx1425.minopp.MinoClient;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.client.player.AbstractClientPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AbstractClientPlayer.class)
 public class AbstractClientPlayerMixin {
 
-    @Inject(method = "getFieldOfViewModifier", at = @At("RETURN"), cancellable = true)
-    public void getFieldOfViewModifier(CallbackInfoReturnable<Float> cir) {
-        cir.setReturnValue(cir.getReturnValueF() * (float) MinoClient.globalFovModifier);
+    @ModifyReturnValue(method = "getFieldOfViewModifier", at = @At("RETURN"))
+    public float getFieldOfViewModifier(float original) {
+        return original * (float) MinoClient.globalFovModifier;
     }
 }
