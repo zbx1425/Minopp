0dc6a11e0eaceee7d30a937006e8327bee889c69
DtHnAme <1904685725@qq.com>
2025-02-06 12:51:23 +0800

    Maid: Show condition description

--- forge/src/main/java/cn/zbx1425/minopp/forge/compat/touhou_little_maid/task/MinoppTask.java
+++ forge/src/main/java/cn/zbx1425/minopp/forge/compat/touhou_little_maid/task/MinoppTask.java
@@ -3,6 +3,8 @@ package cn.zbx1425.minopp.forge.compat.touhou_little_maid.task;
 import cn.zbx1425.minopp.Mino;
 import com.github.tartaricacid.touhoulittlemaid.api.task.IMaidTask;
 import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
+import com.github.tartaricacid.touhoulittlemaid.init.InitSounds;
+import com.github.tartaricacid.touhoulittlemaid.util.SoundUtil;
 import com.google.common.collect.Lists;
 import com.mojang.datafixers.util.Pair;
 import net.minecraft.resources.ResourceLocation;
@@ -12,12 +14,15 @@ import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class MinoppTask implements IMaidTask {
+    public static final ResourceLocation UID = new ResourceLocation(Mino.MOD_ID, "minopp_task");
     @Override
     public @NotNull ResourceLocation getUid() {
-        return new ResourceLocation(Mino.MOD_ID, "minopp_task");
+        return UID;
     }
 
     @Override
@@ -27,8 +32,8 @@ public class MinoppTask implements IMaidTask {
 
     @Nullable
     @Override
-    public SoundEvent getAmbientSound(EntityMaid entityMaid) {
-        return null;
+    public SoundEvent getAmbientSound(EntityMaid maid) {
+        return SoundUtil.environmentSound(maid, InitSounds.MAID_IDLE.get(), 0.5f);
     }
 
     @Override
@@ -46,4 +51,13 @@ public class MinoppTask implements IMaidTask {
 //    public boolean canSitInJoy(@NotNull EntityMaid maid, String joyType) {
 //        return joyType.equals(MinoTable);
 //    }
+
+    @Override
+    public List<Pair<String, Predicate<EntityMaid>>> getConditionDescription(EntityMaid maid) {
+        return Collections.singletonList(Pair.of("has_mino", this::hasMino));
+    }
+
+    private boolean hasMino(EntityMaid maid) {
+        return maid.getMainHandItem().isEmpty() || maid.getMainHandItem().is(Mino.ITEM_HAND_CARDS.get());
+    }
 }
