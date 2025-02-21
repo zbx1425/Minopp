13c506010136927033ae30bfde8e7b80e7da5dca
zbx1425 <support@zbx1425.cn>
2025-02-21 16:37:38 +0800

    Fix EntityAutoPlayer invulnerability

--- common/src/main/java/cn/zbx1425/minopp/entity/EntityAutoPlayer.java
+++ common/src/main/java/cn/zbx1425/minopp/entity/EntityAutoPlayer.java
@@ -18,6 +18,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
@@ -242,7 +243,7 @@ public class EntityAutoPlayer extends LivingEntity {
 
     @Override
     public boolean isInvulnerableTo(DamageSource source) {
-        return true;
+        return isRemoved() || !source.is(DamageTypeTags.BYPASSES_INVULNERABILITY);
     }
 
     @Override
